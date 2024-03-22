8a8b8c047df5db43777bccbd78cbdddcada34142
m1s5 <m1s5.trash@yandex.com>
2024-03-22 11:13:01 +0300

    add community packages

--- plugins/community.lua
+++ plugins/community.lua
@@ -4,6 +4,24 @@ return {
   -- example of importing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
-  -- { import = "astrocommunity.colorscheme.catppuccin" },
-  -- { import = "astrocommunity.completion.copilot-lua-cmp" },
+  { import = "astrocommunity.pack.tailwindcss" },
+  { import = "astrocommunity.pack.rust" },
+  { import = "astrocommunity.pack.typescript" },
+  { import = "astrocommunity.pack.markdown" },
+  { import = "astrocommunity.pack.json" },
+  { import = "astrocommunity.pack.yaml" },
+  { import = "astrocommunity.pack.toml" },
+  { import = "astrocommunity.pack.vue" },
+  { import = "astrocommunity.pack.astro" },
+  { import = "astrocommunity.pack.html-css" },
+  { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.pack.bash" },
+  { import = "astrocommunity.pack.docker" },
+  { import = "astrocommunity.pack.terraform" },
+  { import = "astrocommunity.pack.ansible" },
+  { import = "astrocommunity.pack.lua" },
+  
+  { import = "astrocommunity.scrolling.neoscroll-nvim" },
+  { import = "astrocommunity.completion.codeium-vim" },
+  { import = "astrocommunity.colorscheme.catppuccin" },
 }
